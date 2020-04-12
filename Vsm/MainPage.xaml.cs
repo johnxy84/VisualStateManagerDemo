fed7ca1203ad6030269df8b4456b31aa991d35cf
John Okoroafor <john.okoroafor@westwing.de>
2020-04-12 12:40:33 +0200

    VSM Demo

---
+++ Vsm/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Vsm
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SizeChanged += MainPage_SizeChanged;
+        }
+
+        private void MainPage_SizeChanged(object sender, EventArgs e)
+        {
+            string state = Width > Height ? "Landscape" : "Portrait";
+
+            VisualStateManager.GoToState(MainLayout, state);
+        }
+    }
+}
